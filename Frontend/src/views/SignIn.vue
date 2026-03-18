59bd9f1896038d01548ca0f9c302090d2b952bc3
zynfx1 <sebigan.zyn@gmail.com>
2026-03-19 01:37:00 +0800

    feat: enhance sign-in functionality with JWT authentication and error handling

--- Frontend/src/views/SignIn.vue
+++ Frontend/src/views/SignIn.vue
@@ -11,39 +11,43 @@ import SignUp from './SignUp.vue';
         <img src="../assets/img/logo5.png" alt="" class="w-3/9" />
         <p class="px-5 text-center text-gray-800/50">Drop your campus experience.</p>
       </div>
-      <div class="flex w-full flex-col items-center justify-center gap-5 px-4 sm:px-8">
-        <input
-          class="focus:border-jungle-green-900 bg-email h-15 w-full rounded-xl border-2 border-black/10 bg-size-[auto_25px] bg-position-[left_10px_center] bg-no-repeat px-12 shadow-lg shadow-black/15 focus:outline-none"
-          type="text"
-          placeholder="Email"
-        />
-        <input
-          class="focus:border-jungle-green-900 bg-password h-15 w-full rounded-xl border-2 border-black/10 bg-size-[auto_25px] bg-position-[left_10px_center] bg-no-repeat px-12 shadow-lg shadow-black/15 focus:outline-none"
-          type="text"
-          placeholder="Password"
-        />
-        <button
-          class="bg-jungle-green-900 h-15 w-full cursor-pointer rounded-xl border-2 border-white/10 text-white"
-        >
-          Sign In
-        </button>
-        <a class="text-jungle-green-800 h-full w-full text-center text-sm" href="#"
-          >Forgot Password?</a
-        >
-        <div class="flex h-full w-full items-center justify-center">
-          <div class="h-px flex-1 bg-black/20"></div>
-          <span class="text-md mx-4 text-black/60">OR</span>
-          <div class="h-px flex-1 bg-black/20"></div>
-        </div>
-        <button
-          class="border-jungle-green-600 text-jungle-green-900 h-15 w-full cursor-pointer rounded-xl border-2"
-        >
-          Continue with google
-        </button>
-        <p class="mb-5 h-full w-full text-center text-sm text-black/60" href="#">
-          Don't have an account?
-          <a href="/auth/signup" class="text-jungle-green-900 font-bold">Sign Up</a>
-        </p>
+      <div class="flex w-full items-center justify-center px-4 sm:px-8">
+        <form class="flex h-full w-full flex-col gap-5">
+          <input
+            class="focus:border-jungle-green-900 bg-email h-15 w-full rounded-xl border-2 border-black/10 bg-size-[auto_25px] bg-position-[left_10px_center] bg-no-repeat px-12 shadow-lg shadow-black/15 focus:outline-none"
+            type="text"
+            placeholder="Email"
+            required
+          />
+          <input
+            class="focus:border-jungle-green-900 bg-password h-15 w-full rounded-xl border-2 border-black/10 bg-size-[auto_25px] bg-position-[left_10px_center] bg-no-repeat px-12 shadow-lg shadow-black/15 focus:outline-none"
+            type="text"
+            placeholder="Password"
+            required
+          />
+          <button
+            class="bg-jungle-green-900 h-15 w-full cursor-pointer rounded-xl border-2 border-white/10 text-white"
+          >
+            Sign In
+          </button>
+          <a class="text-jungle-green-800 h-full w-full text-center text-sm" href="#"
+            >Forgot Password?</a
+          >
+          <div class="flex h-full w-full items-center justify-center">
+            <div class="h-px flex-1 bg-black/20"></div>
+            <span class="text-md mx-4 text-black/60">OR</span>
+            <div class="h-px flex-1 bg-black/20"></div>
+          </div>
+          <button
+            class="border-jungle-green-600 text-jungle-green-900 h-15 w-full cursor-pointer rounded-xl border-2"
+          >
+            Continue with google
+          </button>
+          <p class="mb-5 h-full w-full text-center text-sm text-black/60" href="#">
+            Don't have an account?
+            <a href="/auth/signup" class="text-jungle-green-900 font-bold">Sign Up</a>
+          </p>
+        </form>
       </div>
     </div>
   </section>
